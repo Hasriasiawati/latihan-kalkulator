14389e8094958b5326140ee6fab35294fba9d9ec
username-kamu <kodingindonesia@gmail.com>
2018-08-08 23:11:33 +0700

    versi 1

---
+++ function/aritmatika.php
@@ -0,0 +1,27 @@
+<?php
+
+	function penjumlahan($x, $y) {
+		$hasil = $x + $y;
+		return $hasil;
+	}
+
+	function pengurangan($x, $y) {
+		$hasil = $x - $y;
+		return $hasil;
+	}
+
+	function perkalian($x, $y) {
+		$hasil = $x * $y;
+		return $hasil;
+	}
+
+	function pembagian($x, $y) {
+		$hasilx = $x / $y;
+		$hasil = round($hasilx, 2);
+
+		return $hasil;
+
+		
+	}
+
+?>
